6ebfeb3ecbdb67f4f0461bd4cf760db2063d4728
Nyandika Donald <donald@pixsel.net>
2018-09-13 17:01:09 +0300

    Move objects to individual files

---
+++ MpesaDaraja/Models/DarajaResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MpesaDaraja.Models
+{
+    public class DarajaResponse
+    {
+        public string OriginatorConverstionID { get; set; }
+        public string ConversationID { get; set; }
+        public string ResponseDescription { get; set; }
+    }
+}

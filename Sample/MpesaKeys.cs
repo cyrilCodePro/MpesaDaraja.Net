4390c599e20d5ad952bf270bfd7d3b85177d22cc
Nyandika Donald <donald@pixsel.net>
2018-09-14 15:33:57 +0300

    Add a sample project to solution

---
+++ Sample/MpesaKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public class MpesaKeys
+    {
+        public string ConsumerKey { get; set; }
+        public string ConsumerSecret { get; set; }
+        public string PassKey { get; set; }
+        public string ShortCode { get; set; }
+    }
+}
